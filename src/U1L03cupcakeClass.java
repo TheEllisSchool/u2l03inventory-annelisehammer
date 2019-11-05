ebf71c9f211a33be4401f456959021a7b84211a0
annelisehammer <annelisehammer@172.16.21.61>
2019-11-05 09:09:39 -0500

    Finished :)

---
+++ src/U1L03cupcakeClass.java
@@ -0,0 +1,10 @@
+
+public class U1L03cupcakeClass {
+
+	public static void main(String[] args) {
+		cupcakeClass elaini = new cupcakeClass ("Elaini Asamere", 24);
+		System.out.println(elaini);
+		
+	}
+
+}
